ca2c4383249a170854645c963c0a8b01d9c30672
Naam Pondpat <naam.pt@gmail.com>
2022-05-07 11:12:25 -0400

    Add project files.

---
+++ WebScraper_Lambda/Function.cs
@@ -0,0 +1,30 @@
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace WebScraper_Lambda;
+
+public class Function
+{
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public string FunctionHandler(MyData data, ILambdaContext context)
+    {
+        return $"Hello {data.Name}".ToUpper();
+    }
+
+    public class MyData
+    {
+        public MyData(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
